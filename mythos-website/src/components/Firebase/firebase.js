4672822f90016de056518355bb4bc66d39c46e83
unknown <svanur123@gmail.com>
2020-09-28 21:06:02 +0000

    firebase login logout create user ready

---
+++ mythos-website/src/components/Firebase/firebase.js
@@ -0,0 +1,37 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+
+var config = {
+  apiKey: "AIzaSyBwolh2i3lfV89ZllJVTVmnG7jQ76fw-4M",
+  authDomain: "mythos-website.firebaseapp.com",
+  databaseURL: "https://mythos-website.firebaseio.com",
+  projectId: "mythos-website",
+  storageBucket: "mythos-website.appspot.com",
+  messagingSenderId: "698140430251",
+  appId: "1:698140430251:web:13f39312136b1eeff20cee"
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+
+    this.auth = app.auth();
+  }
+
+  // *** Auth API ***
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
--- mythos-website/src/hoc/Au/Au.js
+++
@@ -1,3 +0,0 @@
-const au = (props) => props.children;
-
-export default au;
