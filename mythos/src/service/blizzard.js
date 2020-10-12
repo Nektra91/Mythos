3f86d825010121f7b1d2f821f231c43f31c87d23
unknown <svanur123@gmail.com>
2020-10-12 21:17:14 +0000

    Apply function moved to service

--- mythos/src/service/blizzard.js
+++ mythos/src/service/blizzard.js
@@ -2,8 +2,16 @@ import axios from 'axios';
 
 const blizzardFunctions = {
     async getPlayerData(payload) {
-        console.log(payload)
+        let returnData = {
+            playerClass: '',            
+        }
+        await axios.get(`https://eu.api.blizzard.com/profile/wow/character/${payload.server}/${payload.name}?namespace=profile-eu&access_token=USgc2iLpK0Rl4iED63M5cDBl3Hupw0y7Jv`
+        ).then(data => {
+            returnData.playerClass = data.data.character_class.name.en_US;
+        });
+        return returnData;
     }
 }
 
 export default blizzardFunctions
+
--- mythos/src/service/database.js
+++ mythos/src/service/database.js
@@ -20,6 +20,43 @@ const serviceFunctions = {
             classes = res.data.data.Class;
           });
         return classes;
+    },
+
+    async getSpecsForClass(payload) {
+        let specs = [];     
+        await axios.post(`https://light-jackal-86.hasura.app/v1/graphql`, {
+            query: `query MyQuery {
+            Specialization(where: {Recruitments: {Class: {Name: {_eq: "${payload}"}}}}) {
+                Name
+            }
+            }`
+        }).then(response => {
+            specs = response.data.data.Specialization
+        });
+        return specs;
+    },
+
+    async createApplication(payload) {
+        await axios.post(`https://light-jackal-86.hasura.app/v1/graphql`, {
+            query: `mutation insert_Application {
+                insert_Application(objects: {
+                About: "${payload.about}", 
+                BattleTag: "${payload.battletag}", 
+                Brag: "${payload.brag}", 
+                DiscordTag: "${payload.discordtag}", 
+                Name: "${payload.name}", 
+                RaidingExperience: "${payload.experience}", 
+                Role: "${payload.spec}", 
+                Server: "${payload.server}", 
+                WarcraftLogTag: "${payload.log}", 
+                WhyMythos: "${payload.why}"}) {
+                returning {
+                    Id
+                    Name
+                }
+                }
+            }`
+        })
     }
 }
 export default serviceFunctions;
