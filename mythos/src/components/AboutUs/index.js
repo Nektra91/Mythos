1e30a7bd311e6736c70f6c85dc3eb9fa304bc46f
unknown <svanur123@gmail.com>
2020-10-22 20:47:50 +0000

    About us

--- mythos/src/components/AboutUs/index.js
+++ mythos/src/components/AboutUs/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import styles from './about.module.css';
+
 const About = () => (
-  <div>
-    <h1>AboutUs      
-    </h1>
+  <div className={styles.baseContainer}>
+    <div className={styles.container}>
+      <h1 className={styles.alignCenter}>About us</h1>
+      <div className={styles.alignCenter}>
+        <div>
+          <p>Mythos traces its roots back on Sunstrider. 
+             The guild was formed in 2012 as Old by 3 good friends which are all a part of the guild today.
+             It started out as just close friends but over the years we have built up to become an international team of like minded individuals.
+             We've ensured that the guild is based around a strong core of people who know each other and have raided together for a long time.
+          </p>
+          <p>
+             We have always pushed ourselves to our limits in order to achieve the highest possible standard whilst still having a good time all whilst on a 2 day raiding schedule.
+             We aim to push Mythic content to achieve Cutting Edge in Shadowlands all whilst still retaining our 2 day raiding schedule.</p>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
