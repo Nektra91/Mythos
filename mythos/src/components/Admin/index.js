a7a4be6e1edda78f71347271287633beec9329e1
unknown <svanur123@gmail.com>
2020-10-24 17:12:00 +0000

    small setup admin/home

--- mythos/src/components/Admin/index.js
+++ mythos/src/components/Admin/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
- 
-import { withFirebase } from '../Firebase';
+
+import style from './admin.module.css';
  
 class AdminPage extends Component {
   constructor(props) {
@@ -12,59 +12,27 @@ class AdminPage extends Component {
     };
   }
  
-  componentDidMount() {
-    this.setState({ loading: true });
- 
-    this.props.firebase.users().on('value', snapshot => {
-      const usersObject = snapshot.val();
- 
-      const usersList = Object.keys(usersObject).map(key => ({
-        ...usersObject[key],
-        uid: key,
-      }));
- 
-      this.setState({
-        users: usersList,
-        loading: false,
-      });
-    });
-  }
-
-  componentWillUnmount() {
-    this.props.firebase.users().off();
-  }
- 
   render() {
-    const { users, loading } = this.state;
  
     return (
-      <div>
-        <h1>Admin</h1>
- 
-        {loading && <div>Loading ...</div>}
- 
-        <UserList users={users} />
+      <div className={style.baseContainer}>
+        <div className={style.Center}>
+          <h1>Admin</h1>
+        </div>        
+        <div className={style.Row}>
+          <div className={style.Tile}>
+            <span>Users</span>
+          </div>
+          <div className={style.Tile}>
+            <span>Recruitment</span>
+          </div>
+          <div className={style.Tile}>
+            <span>Progress</span>
+          </div>
+        </div>
       </div>
     );
   }
-}
+};
  
-const UserList = ({ users }) => (
-  <ul>
-    {users.map(user => (
-      <li key={user.uid}>
-        <span>
-          <strong>ID:</strong> {user.uid}
-        </span>
-        <span>
-          <strong>E-Mail:</strong> {user.email}
-        </span>
-        <span>
-          <strong>Username:</strong> {user.username}
-        </span>
-      </li>
-    ))}
-  </ul>
-);
- 
-export default withFirebase(AdminPage);
+export default AdminPage;
